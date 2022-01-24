9ae6bde6983c8e709d6d383def9a259fd980b980
alexbieg-skilljar <alex.bieg@skilljar.com>
2022-01-24 15:58:08 -0800

    Initial better palette: Bubbles up recent commands and closes with backspace

--- main.ts
+++ main.ts
@@ -1,137 +1,75 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { FuzzySuggestModal, Plugin, Command, App } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+class OrderedSet<T> extends Set<T> {
+	add(item: T) {
+		if (this.has(item)) {
+			this.delete(item);
+		}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+		return super.add(item);
+	}
+
+	valuesByLastAdd() : T[] {
+		return Array.from(this).reverse()
+	}
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	prevCommands : OrderedSet<Command>;
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		this.prevCommands = new OrderedSet<Command>()
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'toggle-better-commmand-palette',
+			name: 'Toggle better command palette',
+			hotkeys: [{ modifiers: ["Mod", "Shift"], key: "p" }],
 			callback: () => {
-				new SampleModal(this.app).open();
+				new BetterCommandPaletteModal(this.app, this.prevCommands, this).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
 
 	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class BetterCommandPaletteModal extends FuzzySuggestModal<Command> {
+	prevCommands : OrderedSet<Command>;
+	constructor(app: App, prevCommands: OrderedSet<Command>, plugin: Plugin) {
 		super(app);
+		this.prevCommands = prevCommands;
+
+
+		plugin.registerDomEvent(this.inputEl, 'keydown', (event) => {
+			if (event.key === 'Backspace' && event.target.value == '') {
+				this.close()
+			}
+		});
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	getItems() : Command[] {
+		const allAvailableComands = new OrderedSet<Command>(this.app.commands.listCommands())
+
+		for (const command of this.prevCommands.values()) {
+			if (allAvailableComands.has(command)) {
+				// Bring it to the top
+				allAvailableComands.add(command);
+			}
+		}
+
+		return allAvailableComands.valuesByLastAdd();
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	getItemText(item : Command) {
+		return item.name;
 	}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	onChooseItem(item : Command) {
+		this.prevCommands.add(item);
+		this.app.commands.executeCommandById(item.id);
 	}
 }
